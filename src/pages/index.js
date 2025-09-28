de90d92826e05295b97265e63b0dee52eebbee68
itachiszep <itachiszep@int.pl>
2025-09-28 15:14:37 +0200

    added itachi music

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className='center'>
+        <img className="itachi" src="/images/itachi-uchiha-naruto-amoled-black-background-minimal-art-3840x2160-6478.jpg"/>
+<iframe width="853" height="480" src="https://www.youtube.com/embed/7RUylQDHQMk"></iframe>
+<iframe width="853" height="480" src="https://www.youtube.com/embed/q0xc49VGrGc"></iframe>
+<iframe width="853" height="480" src="https://www.youtube.com/embed/EIRLTsjrDQI"></iframe>
+
+      </div>
+    </div>
+  );
+}
