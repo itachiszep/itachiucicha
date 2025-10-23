54624cc790c90b74c7874625b497b4b7364386d2
itachiszep <itachiszep@int.pl>
2025-10-23 18:46:19 +0200

    added music

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,12 @@ export default function Home() {
 <iframe width="853" height="480" src="https://www.youtube.com/embed/CGyEd0aKWZE?si=O3s69_Vd5LjzD2Tj"></iframe>
 <iframe width="853" height="480" src="https://www.youtube.com/embed/BS46C2z5lVE?si=TFgIGA3HhPXOOJHm"></iframe>
 <iframe width="853" height="480" src="https://www.youtube.com/embed/6N2dLghOMro"></iframe>
+
+
+<iframe width="853" height="480" src="https://www.youtube.com/embed/3leVBrkTPuw"></iframe>
+<iframe width="853" height="480" src="https://www.youtube.com/embed/y4-in1XbVEc"></iframe>
+
+
 <iframe width="853" height="480" src="https://www.youtube.com/embed/7RUylQDHQMk"></iframe>
 <iframe width="853" height="480" src="https://www.youtube.com/embed/A-NQaCzzk4o"></iframe>
 <iframe width="853" height="480" src="https://www.youtube.com/embed/q0xc49VGrGc"></iframe>
